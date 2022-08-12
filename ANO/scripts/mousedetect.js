b0c0814d679f193f6d1e2a0753e525744a605efd
Sergey <108625483+Ark1me@users.noreply.github.com>
2022-08-12 14:08:13 +0300

    Update mousedetect.js

--- ANO/scripts/mousedetect.js
+++ ANO/scripts/mousedetect.js
@@ -1,2 +1,20 @@
-let pageY = event.pageY;
-console.log(pageY);
+const arrowUpButton = document.querySelector('.arrow_up');
+
+function meme() {
+  window.addEventListener('scroll', trackScroll);
+
+  function trackScroll() {
+    let scrolled = window.pageYOffset;
+    let coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      arrowUpButton.classList.remove('arrow_up_show');
+    }
+    if (scrolled < coords) {
+      arrowUpButton.classList.add('arrow_up_show');
+    }
+  }
+}
+
+meme();
+
